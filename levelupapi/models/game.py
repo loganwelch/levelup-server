8c0590bc95ae127b3697f46d61c9d3279dd6850c
Logan Welch <loganewelch@gmail.com>
2023-08-15 15:10:01 -0500

    restructured models and json files and added maker, players needed, and skill level for games

--- levelupapi/models/game.py
+++ levelupapi/models/game.py
@@ -4,5 +4,8 @@ from django.db import models
 class Game(models.Model):
     """Game model class"""
     title = models.CharField(max_length=60)
+    maker = models.CharField(max_length=60)
+    number_of_players = models.IntegerField(3)
+    skill_level = models.IntegerField(3)
     creator = models.ForeignKey("Gamer", on_delete=models.CASCADE, related_name="created_games")
     game_type = models.ForeignKey("GameType", on_delete=models.CASCADE, related_name="games")
